bcc456b3219d251d5973689250cf614a82eaec3a
matheusrodrigues1711 <matheusrodrigues1711@gmail.com>
2026-09-16 13:53:11 -0300

    Remove autenticação com GitHub

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('produtos.urls')),
 ]
